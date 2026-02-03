0f635f8fd2504ae3549cc669144408b27cb396c7
Trevor Gruby <tiroknee@gmail.com>
2026-02-03 07:30:11 -0800

    fixing prologue

--- packages/asciitorium/src/catacomb/SplashScreen.tsx
+++ packages/asciitorium/src/catacomb/SplashScreen.tsx
@@ -67,27 +67,22 @@ export const SplashScreen = ({ onComplete }: SplashScreenProps = {}) => {
       <Banner font="marker" letterSpacing={1} text="Prologue" />
 
       {/* prettier-ignore */}
-      <Text width={75} textAlign="top-left" height="fill" typewriter>
-        The ruins of an old mining settlement cling to the rim of a deep and broken valley.
-        Once beautiful and prosperous, it now leaks monsters through the earth
-        like a broken sewer main. Three fissures scar the landscape and seem to be spewing this cesspool of evil: ¶¶
+      <Text width={60} gap={{top: 2}} textAlign="top-left" height="fill" typewriter>
+        Long ago, Tahara was a land of perfect balance, 
+        sustained by a living relic known as the Veilstone.¶¶
 
-        • The Graveditch ¶
-        • The Mine ¶
-        • The Tomb ¶¶
-
-        Rumors claim these places promise treasure and glory. Mostly, it's just death. ¶¶
-
-        Into this bleak landscape wanders a recently graduated bard. While having no
-        possessions of his own, he does carry a strain of optimism that his mother classifies as a medical concern. ¶¶ Arriving
-        at the Lonely Lantern Inn, he spies a sign nailed to the outer wall: BARDS WANTED! ¶¶
-
-        An unusual policy for a place with no visible prospects of commerce... but then again, thats why they need a bard! ¶¶
-        Pushing aside a bothersome thought about the need for bards - in plural - he knocks on the door...
+        When an ancient force of chaos shattered that balance, 
+        and guardians who protected it were lost, 
+        the catacombs beneath Tahara were corrupted.¶¶
+        
+        Now the Veilstone weakens, feeding a spreading rot known as the Withering. 
+        Drawn by fate or desperation, you fall into the catacombs below.¶¶
+        Restore the balance… or be consumed by the dark.¶¶
+        your story begins now.
       </Text>
 
       <Text textAlign="bottom" gap={2}>
-        Press [Enter] to enter the Lonely Lantern Inn...
+        Press [Enter] ...
       </Text>
     </Column>
   );
